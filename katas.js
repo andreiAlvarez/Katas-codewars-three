50282c339026fcd98264db8627ea37524d2901b6
Andrei Alvarez <62512410+andreiAlvarez@users.noreply.github.com>
2021-02-13 12:15:10 -0500

    Update katas.js

--- katas.js
+++ katas.js
@@ -1,3 +1,9 @@
+//Kata 1
+
 const gearInchCalculator = (chainrings, sprockets) => chainrings.map(c =>
     sprockets.map(sprocket => Math.round(+((26 * c) / sprocket) * 10) / 10)
   );
+
+//Kata 2
+
+const gatesCalculator = (num1, num2) => `%${(num1 / num2) * 100}`;

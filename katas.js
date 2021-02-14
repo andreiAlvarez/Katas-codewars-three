141a8e9617cf22a7ac46cb766c37cf5c393ede4f
Andrei Alvarez <62512410+andreiAlvarez@users.noreply.github.com>
2021-02-14 09:24:41 -0500

    Update katas.js

--- katas.js
+++ katas.js
@@ -7,3 +7,18 @@ const gearInchCalculator = (chainrings, sprockets) => chainrings.map(c =>
 //Kata 2
 
 const gatesCalculator = (num1, num2) => `%${(num1 / num2) * 100}`;
+
+//Kata 3
+
+function topThreeWords(text) {
+ const arr = []
+  text = text.toLowerCase().split` `.map(v=>{
+    if (v===`'`) return ''
+    return v.replace(/[^'a-z]/gi,'')
+  }).filter(v=>v)
+  const obj = text.reduce((a,b)=>(a[b]=a[b]?a[b]+1:1,a),{})
+  for (let i in obj){
+    arr.push([i,obj[i]])
+  }
+  return arr.sort((a,b)=>b[1]-a[1]).slice(0,3).map(v=>v[0])
+}

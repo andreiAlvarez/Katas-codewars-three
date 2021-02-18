ce86ce76d0e61403c379ede55a8914019ccee1e4
Andrei Alvarez <62512410+andreiAlvarez@users.noreply.github.com>
2021-02-18 08:51:06 -0500

    Update katas.js

--- katas.js
+++ katas.js
@@ -86,3 +86,23 @@ function drive(drinks, finished, drive_time) {
   time1+=(units.toString().split`.`[0]*60*60*1000+(units.toString().split`.`[1]||0)*0.6*60*1000)
   return [units,time1<time2]
 };
+
+// kata 6
+
+function alphabetWar(fight) {
+  let dict = { w: 4, p: 3, b: 2, s: 1, m: -4, q: -3, d: -2, z: -1 };
+
+  const arr = [];
+  const arr1 = fight.split("");
+  for (let i = 0; i < fight.length; i++) {
+    if (arr1[i - 1] !== "*" && arr1[i] !== "*" && arr1[i + 1] !== "*") {
+      arr.push(arr1[i]);
+    }
+  }
+  let sum = arr.reduce((a, b) => a + (dict[b] ? dict[b] : 0), 0);
+  return sum < 0
+    ? "Right side wins!"
+    : sum > 0
+    ? "Left side wins!"
+    : "Let's fight again!";
+};

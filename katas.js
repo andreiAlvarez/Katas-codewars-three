00cffc913e5062049e94002e208bb7ba531e2b7c
Andrei Alvarez <62512410+andreiAlvarez@users.noreply.github.com>
2021-02-26 18:58:11 -0600

    Update katas.js

--- katas.js
+++ katas.js
@@ -168,3 +168,16 @@ function titleCase(title, minorWords = '') {
   // kata 11
     
   const tetrahedron = size => (size * (size + 1) * (size + 2)) / 6; 
+    
+    
+    // kata 12 
+    
+    function arraysSimilar(arr1, arr2) {
+    arr1 = arr1.sort((a, b) => a - b);
+  arr2 = arr2.sort((a, b) => a - b);
+  return arr1.length > arr2.length
+    ? arr1.every((v, i) => v === arr2[i])
+    : arr2.every((v, i) => v === arr1[i]);
+}; 
+    
+    

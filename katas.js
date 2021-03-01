b6e3644a8045e8896f858a3beb4343a682efef08
Andrei Alvarez <62512410+andreiAlvarez@users.noreply.github.com>
2021-02-28 20:08:09 -0500

    Update katas.js

--- katas.js
+++ katas.js
@@ -180,4 +180,13 @@ function titleCase(title, minorWords = '') {
     : arr2.every((v, i) => v === arr1[i]);
 }; 
     
+    // kata 13
     
+    function calculate1RM(w, r) {
+  let a = w * (1 + r / 30);
+  let b = (100 * w) / (101.3 - 2.67123 * r);
+  let c = w * r ** 0.1;
+  if (r === 0 || w === 0) return 0;
+  if (r === 1) return w;
+  return Math.round(Math.max(a, b, c));
+};

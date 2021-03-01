653e403c47c3c6c06e6999dfc6b44027a6e485a6
Andrei Alvarez <62512410+andreiAlvarez@users.noreply.github.com>
2021-02-28 20:09:42 -0500

    Update katas.js

--- katas.js
+++ katas.js
@@ -190,3 +190,7 @@ function titleCase(title, minorWords = '') {
   if (r === 1) return w;
   return Math.round(Math.max(a, b, c));
 };
+
+    //solution 2
+    
+const calculate1RM = (w, r) => r == 1 ? w : r == 0 ? 0 : Math.round(Math.max(...["w*(1+r/30)", "(100*w)/(101.3-2.67123*r)", "w*(r**0.10)"].map(a => eval(a))));
